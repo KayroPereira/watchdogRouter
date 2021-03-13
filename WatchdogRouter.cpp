e20facf0486a1d0331643ab546efad076b6ea8c0
Kayro Pereira <kayrofellyxbr@gmail.com>
2021-03-12 22:31:14 -0300

    Initial commit

---
+++ WatchdogRouter.cpp
@@ -0,0 +1,9 @@
+#include "WatchdogRouter.h"
+
+void setup() {
+
+}
+
+void loop() {
+
+}
---
+++ WatchdogRouter.h
@@ -0,0 +1,6 @@
+
+#ifndef _testEspFirebaseCallback_H_
+	#define _testEspFirebaseCallback_H_
+
+
+#endif /* _testEspFirebaseCallback_H_ */
